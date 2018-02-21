09550b3a19cbec74cfd0223fbf421c881f884ec8
Oscar Castro <ocastro.ji@gmail.com>
2018-02-20 23:46:17 -0400

    Bootstrap Template Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'contactus' => 'pages#contactus'
+  get 'contact' => 'pages#contact'
 
   # For details on the DomainSpecificLanguage available within this file, see http://guides.rubyonrails.org/routing.html
 end
